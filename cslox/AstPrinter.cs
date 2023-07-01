9acc04e6ed74c2f799b1d66aa4d982f0631dd6b0
varblane <varblane@email.cz>
2023-07-01 23:31:47 +0200

    representing code

---
+++ cslox/AstPrinter.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+namespace cslox
+{
+    internal class AstPrinter : IVisitor<string>
+    {
+        internal string Print(Expr expr)
+        {
+            return expr.Accept(this);
+        }
+
+        public string VisitBinaryExpr(Binary expr)
+        {
+            return Parenthesize(expr.op.lexeme, expr.left, expr.right);
+        }
+
+        public string VisitGroupingExpr(Grouping expr)
+        {
+            return Parenthesize("group", expr.expression);
+        }
+
+        public string VisitLiteralExpr(Literal expr)
+        {
+            return expr.value?.ToString() ?? "nil";
+        }
+
+        public string VisitUnaryExpr(Unary expr)
+        {
+            return Parenthesize(expr.op.lexeme, expr.right);
+        }
+
+        private string Parenthesize(string name, params Expr[] exprs)
+        {
+            var builder = new StringBuilder();
+            builder.Append('(').Append(name);
+            foreach (var expr in exprs)
+            {
+                builder.Append(' ');
+                builder.Append(expr.Accept(this));
+            }
+            builder.Append(')');
+            return builder.ToString();
+        }
+    }
+}
